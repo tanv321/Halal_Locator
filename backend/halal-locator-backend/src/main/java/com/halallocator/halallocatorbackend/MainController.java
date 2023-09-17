675427c2185295dedfa3f014eefa2582d5807952
tanv321 <tanvalam321@gmail.com>
2023-09-17 11:11:33 -0400

    cors server side

--- backend/halal-locator-backend/src/main/java/com/halallocator/halallocatorbackend/MainController.java
+++ backend/halal-locator-backend/src/main/java/com/halallocator/halallocatorbackend/MainController.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 public class MainController {
 
-    @GetMapping("localhost:8080")
-    public String home() {
-        return "Welcome to the Spring Boot Application!";
-    }
+    
     @GetMapping("/")
     public String home2() {
         return "Welcome to the Spring Boot Application!";
     }
+    @GetMapping("/test/page")
+    public String homePage(){
+        return "This is the test page";
+    }
+
 }
